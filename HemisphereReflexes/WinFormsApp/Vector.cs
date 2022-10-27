aa91bdd66d5fad3365da8341333d9c4dfded3707
Dominik Debinski <debinskid16@gmail.com>
2022-10-27 23:18:09 +0200

    Add base data structures for points

---
+++ HemisphereReflexes/WinFormsApp/Vector.cs
@@ -0,0 +1,24 @@
+﻿namespace WinFormsApp;
+
+public class Vector3D
+{
+    private double _x;
+    private double _y;
+    private double _z;
+
+    public Vector3D(double x, double y, double z)
+    {
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+
+    public double X => _x;
+    public double Y => _y;
+    public double Z => _z;
+
+    public override string ToString()
+    {
+        return $"[{_x}, {_y}, {_z}]";
+    }
+}
